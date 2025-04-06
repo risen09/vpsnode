201a688fea3334f6424b54d2fa04766d0e2ffea5
risen09 <bayanov_m09@mail.ru>
2025-04-06 17:23:11 +0700

    clear file

--- .vscode-server/data/User/History/5e9d47ed/OMRQ.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-// Генерация 64-байтового ключа (512 бит) в Base64
-const generateSecret = () => {
-  return crypto.randomBytes(64).toString('base64');
-};
-
-const JWT_SECRET = generateSecret();
-console.log('Ваш секретный ключ:', JWT_SECRET);
