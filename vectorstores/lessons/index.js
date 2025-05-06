eafa09bfcbe69bacd7d57bf8fe92f24be3dfb8b2
Ruslan Zagitov <rr.zagitov.02@gmail.com>
2025-05-06 12:41:15 +0300

    feat: add lesson creator agent
    - Introduced a new lesson creator agent that generates lesson structures and content based on RAG search.

---
+++ vectorstores/lessons/index.js
@@ -0,0 +1,45 @@
+const { DirectoryLoader } = require("langchain/document_loaders/fs/directory");
+const { PDFLoader } = require('@langchain/community/document_loaders/fs/pdf')
+const { OllamaEmbeddings } = require('@langchain/ollama');
+const { Chroma } = require("@langchain/community/vectorstores/chroma");
+const { RecursiveCharacterTextSplitter } = require("@langchain/textsplitters");
+
+async function main() {
+  const directoryLoader = new DirectoryLoader('../assets/documents/algebra', {
+    ".pdf": (filePath) => new PDFLoader(filePath, { splitPages: false })
+  })
+  const docs = await directoryLoader.load();
+
+  console.log(`[VectorStore] Found ${docs.length} documents`);
+
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+  });
+
+  const chunks = await splitter.splitDocuments(docs);
+  console.log(`[VectorStore] Split ${docs.length} documents into ${chunks.length} chunks`)
+
+  const cleanedChunks = chunks.map(chunk => ({
+    ...chunk,
+    metadata: {
+      source: chunk.metadata.source,
+      loc_lines_from: chunk.metadata.loc?.lines?.from,
+      loc_lines_to: chunk.metadata.loc?.lines?.to,
+    }
+  }));
+
+  const embeddings = new OllamaEmbeddings({
+    model: "nomic-embed-text",
+  })
+
+  await Chroma.fromDocuments(cleanedChunks, embeddings, {
+    collectionName: "textbooks",
+    url: "http://localhost:8000", // Make sure this points to your ChromaDB instance
+  });
+
+  console.log(`[VectorStore] Added ${cleanedChunks.length} documents to vector store`)
+
+}
+
+main();
